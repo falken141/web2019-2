a48a3fc1e23d8d4ab6d96e99701202624b3c4b4a
falken141 <zm18203@josai.ac.jp>
2019-11-12 03:18:53 +0000

    11/12 kadai

--- ex6-5.js
+++ ex6-5.js
@@ -3,10 +3,16 @@ function sayhello65(){
     alert('Hello,' + who + '!');
 }
 
+function gakita65(){
+    alert('が来た！');
+}
+
 function taro65(){
+    alert('太郎');
     who = '太郎';
 }
 
 function hanako65(){
+    alert('花子');
     who = '花子';
 }
